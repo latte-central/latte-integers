a20ce82732c84e5c7844b68e9735ed092060f359
Fredokun <fredokun@github.com>
2016-08-05 16:27:24 +0200

    version bump, but latte-sets dependency to add

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject latte-integers "0.0.1"
+(defproject latte-integers "0.0.2"
   :description "A formalization of integers in LaTTe."
   :url "https://github.com/fredokun/latte-integers.git"
   :license {:name "MIT Licence"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [latte "0.1.1-SNAPSHOT"]]
+                 [latte "0.2.0-SNAPSHOT"]]
   :codox {:metadata {:doc/format :markdown}
           :namespaces [latte-integers.core]}
   :plugins [[lein-codox "0.9.5"]])

72d24abb3e7bcdd9c92020608ec0d711bd242eb2
fredokun <Frederic.Peschanski@lip6.fr>
2016-12-13 00:05:36 +0100

    version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject latte-integers "0.1.2-SNAPSHOT"
+(defproject latte-integers "0.2.1-SNAPSHOT"
   :description "A formalization of integers in LaTTe."
   :url "https://github.com/fredokun/latte-integers.git"
   :license {:name "MIT Licence"

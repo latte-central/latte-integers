59649768acaf06a59d4364fab8e8b464612e4b59
fredokun <frederic.peschanski@lip6.fr>
2017-11-20 15:10:45 +0100

    version bump

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject latte-integers "0.6.0-SNAPSHOT"
+(defproject latte-integers "0.6.1-SNAPSHOT"
   :description "A formalization of integers in LaTTe."
   :url "https://github.com/fredokun/latte-integers.git"
   :license {:name "MIT Licence"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.9.0-RC1"]
-                 [latte "0.99.9-SNAPSHOT"]
-                 [latte-sets "0.4.1-SNAPSHOT"]]
+                 [latte "0.100.0-SNAPSHOT"]
+                 [latte-sets "0.4.2-SNAPSHOT"]]
   :codox {:output-path "docs/"
           :metadata {:doc/format :markdown}
           :namespaces [latte-integers.core latte-integers.nat
